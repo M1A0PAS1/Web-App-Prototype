4cfa469193d58c54f8aed5445751269ea3aa18c7
M1A0PAS1 <xwan2722@uni.sydney.edu.au>
2023-06-06 19:27:13 +0800

    DOM success,css needed

---
+++ public/script.js
@@ -0,0 +1,22 @@
+
+
+function addRecoard(){
+    //Get DOM Element
+    const exerciseName = document.getElementById('exercise-name').value;
+    const bodyPart = document.getElementById('body-part').value;
+    const weight = document.getElementById('weight').value;
+    const repetition = document.getElementById('repetition').value;
+    const feeling = document.getElementById('feeling').value;
+    const cardContainer = document.getElementById('card-container');
+    //Begin to create card in html
+    const card = document.createElement('div');
+    card.classList.add('card');
+    //Define what will be in the card
+    card.innerHTML = `
+      <h1>${exerciseName}</h1>
+      <p>:${weight}kg</p>
+      <p>:${repetition}</p>
+      <p>:${feeling}</p>
+    `; 
+    cardContainer.appendChild(card);
+}
