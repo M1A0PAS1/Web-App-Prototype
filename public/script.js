3e867fb518f2aeb8afeeadf6d4543801f8287699
M1A0PAS1 <xwan2722@uni.sydney.edu.au>
2023-06-06 21:08:37 +0800

    All done

--- public/script.js
+++ public/script.js
@@ -1,6 +1,16 @@
-
+const bodyPartImages = {
+    chest: 'chest.jpg',
+    shoulders: 'shoulders.jpg',
+    back: 'back.jpg',
+    arms: 'arms.jpg',
+    legs: 'legs.jpg',
+  };
 
 function addRecoard(){
+    //Get current date in DMY format
+    const date = new Date();
+    //+1 because month starts at 0
+    const dateGroup = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
     //Get DOM Element
     const exerciseName = document.getElementById('exercise-name').value;
     const bodyPart = document.getElementById('body-part').value;
@@ -14,6 +24,7 @@ function addRecoard(){
     //Define what will be in the card
     card.innerHTML = `
       <button onclick="showCardData(this)">Detail</button>
+      <img src="${bodyPartImages[bodyPart]}" />
       <h1>${exerciseName}</h1>
       <p>Weight: ${weight}kg</p>
       <p>Repetition:${repetition}</p>
@@ -30,6 +41,7 @@ function addRecoard(){
     card.setAttribute('data-weight', weight);
     card.setAttribute('data-repetition', repetition);
     card.setAttribute('data-feeling', feeling);
+    card.setAttribute('data-date', dateGroup);
 
     //Call the save data to local function
     saveData();
@@ -45,8 +57,10 @@ function saveData(){
         const weight = card.getAttribute('data-weight');
         const repetition = card.getAttribute('data-repetition');
         const feeling = card.getAttribute('data-feeling');
+        const date = card.getAttribute('data-date');
          //add data to the list
         dataSet.push({
+            date,
             exerciseName,
             bodyPart,
             weight,
@@ -66,6 +80,7 @@ function showSummary(){
         data.forEach(function(recoard){
             summary += '<div>';
             summary += '<h1>' + recoard.exerciseName + '</h1>';
+            summary += '<p>' + recoard.date + '</p>';
             summary += '<p>' + recoard.bodyPart + '</p>';
             summary += '<p>' + recoard.weight + '</p>';
             summary += '<p>' + recoard.repetition + '</p>';
@@ -91,12 +106,14 @@ function showCardData(button) {
     const weight = card.getAttribute('data-weight');
     const repetition = card.getAttribute('data-repetition');
     const feeling = card.getAttribute('data-feeling');
+    const date = card.getAttribute('data-date');
     //alert a pop up window
     alert(`
-      exerciseName: ${exerciseName}
-      bodyPart: ${bodyPart}
-      weight: ${weight}kg
-      repetition: ${repetition}
-      feeling: ${feeling}
+      Date: ${date}
+      ExerciseName: ${exerciseName}
+      BodyPart: ${bodyPart}
+      Weight: ${weight}kg
+      Repetition: ${repetition}
+      Feeling: ${feeling}
     `);
 }
